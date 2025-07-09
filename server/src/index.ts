7520693506e3c5e9649e1ecb20e8dd3eb8413923
Kush Kansal <mrkushkansal@gmail.com>
2025-07-09 17:48:59 +0530

    server fix

--- server/src/index.ts
+++ server/src/index.ts
@@ -47,15 +47,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan("dev"));
 
-// Add OPTIONS handler for preflight requests
-app.options("*", (req, res) => {
-  res.header("Access-Control-Allow-Origin", req.headers.origin);
-  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
-  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  res.header("Access-Control-Allow-Credentials", "true");
-  res.sendStatus(200);
-});
-
 app.use(
   session({
     store: MongoStore.create({
